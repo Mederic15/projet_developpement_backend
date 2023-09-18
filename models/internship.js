8b19c47712de6193668a45887020e31cb6e62697
Youva Chaou <youvachaou1234@gmail.com>
2023-09-18 14:44:22 -0400

    model internships modifie

--- models/internship.js
+++ models/internship.js
@@ -4,10 +4,10 @@ const Schema = mongoose.Schema;
 const internshipSchema = new Schema({
     title:{type: String, required: true},
     description: {type: String, required: true},
-    salary: {type: String, required: true},
+    salary: {type: Number, required: true},
     address: {type: String, required: true},
-    startingDate: {type: String, required: true},
-    endingDate: {type: String, required: true},
+    startingDate: {type: Date, required: true},
+    endingDate: {type: Date, required: true},
     employerId:{type: mongoose.Types.ObjectId, required: true, ref:"Employer"}
 });
 
