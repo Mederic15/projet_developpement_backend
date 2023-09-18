166ab7027ca7d2bb5318f0eabb26de6dcd5faf35
Guillaume Giguère <2185208@cmontmorency.qc.ca>
2023-09-18 14:56:57 -0400

    Employer and student controller

---
+++ controllers/employer-controller.js
@@ -0,0 +1,38 @@
+const HttpError = require("../models/http-error");
+const Employer = require("../models/employer");
+
+//GET Methods
+//POST Methods
+async function addEmployer(req, res, next) {
+    const {
+        companyName,
+        address,
+        managerFirstName,
+        managerLastName,
+        phoneNumber,
+        phoneBooth
+    } = req.body;
+    try {
+      const employerToAdd = new Employer({
+            companyName,
+            address,
+            managerFirstName,
+            managerLastName,
+            phoneNumber,
+            phoneBooth
+      });
+      await employerToAdd.save();
+      res
+        .status(201)
+        .json({ employer: employerToAdd.toObject({ getters: true }) });
+    } catch (err) {
+      console.log(err);
+      return next(new HttpError("Error while creating employer", 500));
+    }
+  }
+//PATCH Methods
+//DELETE Methods
+
+module.exports = {
+    addEmployer: addEmployer,
+  };
