2d4036545928c64f6383bf4b0ff156f58b78330c
Youva Chaou <youvachaou1234@gmail.com>
2023-09-18 14:46:48 -0400

    Ajout de la fonction addInternship

--- controllers/internship-controller.js
+++ controllers/internship-controller.js
@@ -1,4 +1,41 @@
 const HttpError = require("../models/http-error");
-const Student = require("../models/student");
 const Internship = require("../models/internship");
 
+//GET Methods
+//POST Methods
+async function addInternship(req, res, next) {
+  const {
+    title,
+    description,
+    salary,
+    address,
+    startingDate,
+    endingDate,
+    employerId
+  } = req.body;
+  try {
+    const internshipToAdd = new Internship({
+        title,
+        description,
+        salary,
+        address,
+        startingDate,
+        endingDate,
+        employerId
+    });
+    await internshipToAdd.save();
+    res
+      .status(201)
+      .json({ internship: internshipToAdd.toObject({ getters: true }) });
+  } catch (err) {
+    console.log(err);
+    return next(new HttpError("Error while creating internship", 500));
+  }
+}
+
+//PATCH Methods
+//DELETE Methods
+
+module.exports = {
+    addInternship: addInternship,
+  };
