ccb31fb35dca73269f1ac8e98b36385bb87690de
Guillaume Giguère <2185208@cmontmorency.qc.ca>
2023-10-01 18:27:16 -0400

    salaire

--- models/internship.js
+++ models/internship.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const internshipSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  salary: { type: Number, required: true },
+  salary: { type: String, required: true },
   address: { type: String, required: true },
   startingDate: { type: Date, required: true },
   endingDate: { type: Date, required: true },
