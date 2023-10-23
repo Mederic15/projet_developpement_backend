75376722c42e0f84f7d8ca0b184895fb7e5c59d9
Youva Chaou <youvachaou1234@gmail.com>
2023-10-23 15:53:56 -0400

    sa

--- routes/employer-routes.js
+++ routes/employer-routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 //Mettre les route selon la demande
 router.post("/employers", controllerEmployer.addEmployer);
 
-router.post("/employers", controllerEmployer.employerConnection);
+router.patch("/employers", controllerEmployer.employerConnection);
 //router.patch("/studentId", controllerStudent.patchStudent);
 
 module.exports = router;
